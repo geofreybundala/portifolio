728851492d3c1bd84cb69cb9eae379f9194007c5
geofreybundala <bundalageofrey2@gmail.com>
2022-04-13 22:39:57 +0300

    👌 IMPROVE:  show articles

--- src/components/Home/Cover.tsx
+++ src/components/Home/Cover.tsx
@@ -1,32 +1,38 @@
-import MyCover from '../../assets/images/me.png'
-import { AiOutlineLinkedin , AiOutlineGithub} from 'react-icons/ai';
+import MyCover from '../../assets/images/me.png';
+import { AiOutlineLinkedin, AiOutlineGithub } from 'react-icons/ai';
 import { FiTwitter } from 'react-icons/fi';
 
 export const Cover = () => {
-    return (
-        <section className='d-flex justify-content-between container welcome-cover'>
-             <div className="cover-image">
-               <img src={MyCover} alt=''/>
-           </div>
+  return (
+    <section className="d-flex justify-content-between container welcome-cover">
+      <div className="cover">
+        <img src={MyCover} alt=""  className='cover-image'/>
+      </div>
 
-           <div className="my-auto mx-auto">
-               <h5 className='text-gray'>Hello, My name is </h5>
-               <h3>Geofrey Bundala</h3>
-               <p className="text-gray">
-                   I'm fullstack developer based in Tanzania,<br/>Let`s create build together 🤝
-               </p>
+      <div className="my-auto mx-auto">
+        <h5 className="text-gray">Hello, My name is </h5>
+        <h3>Geofrey Bundala</h3>
+        <p className="text-gray">
+          I'm fullstack developer based in Tanzania,
+          <br />
+          Let`s create together 🤝
+        </p>
 
-               <div className="mx-4 mb-2 social-link d-flex justify-content-between">
-                    <span><FiTwitter size="23px"/></span>
-                    <span><AiOutlineGithub size="23px"/></span>
-                    <span><AiOutlineLinkedin size="23px"/></span>
-               </div>
-               <button className="btn btn-round p-blue-btn">Download my Resume</button>
-           </div>
+        <div className="mx-4 mb-2 social-link d-flex justify-content-between">
+          <span>
+            <FiTwitter size="23px" />
+          </span>
+          <span>
+            <AiOutlineGithub size="23px" />
+          </span>
+          <span>
+            <AiOutlineLinkedin size="23px" />
+          </span>
+        </div>
+        <button className="btn btn-round p-blue-btn">Download my Resume</button>
+      </div>
+    </section>
+  );
+};
 
-          
-        </section>
-    )
-}
-
-export default Cover
+export default Cover;
